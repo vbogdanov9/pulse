fe4887886b65602b19f66f2c9c596adc8bc273d1
Seva <vbogdanov9@gmail.com>
2024-02-06 22:42:49 +0300

    l53, try javascript

---
+++ src/js/script.js
@@ -0,0 +1,55 @@
+
+
+// var name =  "Seva";
+// let number = 7;
+// const pi = 3.14;
+
+// let answer = confirm("Вам есть 18?");
+// console.log(answer);
+
+
+// let answer = prompt("Вам есть 18?", "");
+// console.log(answer);
+// console.log(4+4);
+
+// let isChecked = false,
+//     isClose = false;
+
+// console.log(isChecked && isClose);
+//  console.log(isChecked || isClose);
+
+// if (2*6 == 8*1) {
+//     console.log('Верно');
+// } else {
+//     console.log('Ошибка');
+// }
+
+
+// let answer = confirm("Вам есть 18?");
+// console.log(answer);
+// if (answer) {
+//     console.log('есть 18');
+// } else {
+//     console.log('нет 18ти');
+// }
+
+
+// const num = 50;
+// if (num < 49) {
+//     console.log('неправильно');
+// } else  if (num > 100) {
+//     console.log('много');
+// } else {
+//     console.log('верно');
+// }
+
+// for (let i=1; i<8; i++) {
+//     console.log(i);
+// }
+
+function logging(a,b) {
+    console.log(a+b)
+}
+
+logging(3,5);
+logging(6,8);
