dd0b2b36a9cfae9b0cff9879a417853b7245bbaf
Seva <vbogdanov9@gmail.com>
2024-02-08 01:17:36 +0300

    l55, add slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,55 +1,19 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({        
+        speed: 600,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg" alt=""></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                dots: true,
+                arrows: false
+              }
+            }
+        ]
+      });
+  });
 
-
-// var name =  "Seva";
-// let number = 7;
-// const pi = 3.14;
-
-// let answer = confirm("Вам есть 18?");
-// console.log(answer);
-
-
-// let answer = prompt("Вам есть 18?", "");
-// console.log(answer);
-// console.log(4+4);
-
-// let isChecked = false,
-//     isClose = false;
-
-// console.log(isChecked && isClose);
-//  console.log(isChecked || isClose);
-
-// if (2*6 == 8*1) {
-//     console.log('Верно');
-// } else {
-//     console.log('Ошибка');
-// }
-
-
-// let answer = confirm("Вам есть 18?");
-// console.log(answer);
-// if (answer) {
-//     console.log('есть 18');
-// } else {
-//     console.log('нет 18ти');
-// }
-
-
-// const num = 50;
-// if (num < 49) {
-//     console.log('неправильно');
-// } else  if (num > 100) {
-//     console.log('много');
-// } else {
-//     console.log('верно');
-// }
-
-// for (let i=1; i<8; i++) {
-//     console.log(i);
-// }
-
-function logging(a,b) {
-    console.log(a+b)
-}
-
-logging(3,5);
-logging(6,8);
+    
