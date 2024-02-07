b3df23ca4b97378d91219972cce56c012494db0e
Seva <vbogdanov9@gmail.com>
2024-02-08 01:50:25 +0300

    fix paths

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({        
         speed: 600,
         // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg" alt=""></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg" alt=""></button>',
         responsive: [
             {
               breakpoint: 992,
