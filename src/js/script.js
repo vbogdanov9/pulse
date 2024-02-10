54b567047b2ca657d1c85ea3e766b7c54c4ff39b
Seva <vbogdanov9@gmail.com>
2024-02-10 23:04:04 +0300

    add tabs, toggle slides

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-// $(document).ready(function(){
+
 //     $('.carousel__inner').slick({        
 //         speed: 600,
 //         // adaptiveHeight: true,
@@ -14,9 +14,11 @@
 //             }
 //         ]
 //       });
-//   });
 
-    
+
+
+
+// слайдер    
 const slider = tns({
   container: '.carousel__inner',
   items: 1,
@@ -32,4 +34,35 @@ document.querySelector('.prev').addEventListener('click', function () {
 
 document.querySelector('.next').addEventListener('click', function () {
   slider.goTo('next');
-});
+});  
+
+
+$(document).ready(function(){
+
+// Табы   
+$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+// переключение карточек
+function toggleSlide(item) {
+  $(item).each(function(i) {
+    $(this).on('click', function(e) {
+      e.preventDefault();
+      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    });
+  });
+}
+
+toggleSlide('.catalog-item__link');
+toggleSlide('.catalog-item__back');
+
+
+
+}); 
+
+
+
