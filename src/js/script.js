7988075fab44e854bd84e8f65b09709459832d54
Seva <vbogdanov9@gmail.com>
2024-02-14 23:23:16 +0300

    fix catalog bug

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,3 @@
-
-//     $('.carousel__inner').slick({        
-//         speed: 600,
-//         // adaptiveHeight: true,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg" alt=""></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg" alt=""></button>',
-//         responsive: [
-//             {
-//               breakpoint: 992,
-//               settings: {
-//                 dots: true,
-//                 arrows: false
-//               }
-//             }
-//         ]
-//       });
-
-
-
-
-
-
-
 $(document).ready(function(){
 
     // слайдер    
